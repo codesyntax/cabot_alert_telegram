bb3c58a73cdd41dccdcd912ac2691372bab6fe1d
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-05-29 09:17:53 +0200

    Back to development: 0.7
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot_alert_telegram',
-      version='0.6',
+      version='0.7.dev0',
       description='A Telegram alert plugin for Cabot by Arachnys',
       long_description=open('README.rst').read() + '\n' +
         open('HISTORY.txt').read(),
